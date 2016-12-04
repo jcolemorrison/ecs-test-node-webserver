95f46f53d18e5e6bbad9f65a432f3474df356034
J Cole Morrison <j.cole.morrison@gmail.com>
2016-12-03 18:21:39 -0800

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+// MODULES
+var express = require('express');
+
+// EXPRESS CONFIG
+var app = express();
+
+app.set('view engine', 'pug')
+app.set('port', 5000);
+
+// set up "ping" endpoint
+app.get('/web', function (req, res) {
+  res.send('server')
+});
+
+// catch/handle 404
+app.use(function (req, res, next) {
+  res.status(404).send('404')
+});
+
+// catch/handle 500
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('500')
+});
+
+module.exports = app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
---
+++ app.spec.js
@@ -0,0 +1,15 @@
+var expect = require("chai").expect;
+var request = require('supertest');
+process.env.NODE_ENV = 'test';
+
+var app = require('./app.js');
+
+describe("Routing", () => {
+  describe("web", () => {
+    it("GET /web returns server", (done) => {
+      request(app)
+        .get('/web')
+        .expect(200, 'server', done);
+    });
+  });
+});
